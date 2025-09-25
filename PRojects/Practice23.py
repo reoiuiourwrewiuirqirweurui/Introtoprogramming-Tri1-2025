16ea036dbb23ef1dafc14cf37723f3b0dca266c1
reoiuiourwrewiuirqirweurui <821318@stma.k12.mn.us>
2025-09-25 14:03:28 -0500

    Help me :crying_emoji:

--- PRojects/Practice23.py
+++ PRojects/Practice23.py
@@ -9,6 +9,18 @@ snake_case = True
 SCREAMING_SNAKE_CASE = False
 #Rules
 #1. Shit should be clear and short as possible.
+input("what's your name??")
+if input:("what is your name?\n> ")
+print ("Jackie phan")
+#get the number as a string
+print("complete")
+Num = input("what number do you want to square?\n ")
+if input: ("Num")
+#Parse (convert) the string to integer (cast)
+Num = int(Num)
+#do math and print
+print(Num * Num)
+
 
 
 
---
+++ PRojects/SpaceShuttle.py
@@ -0,0 +1,23 @@
+snake_case = True
+countdown = input ("How many seconds left until flying?\n> ")
+cooldown = int(countdown)
+print("Bye have a nice time")
+#supply lists
+OxygenTanks = input("How many tanks do you have?")
+OxygenTanks = int(OxygenTanks)
+print("Your a noob!")
+food_pack = input("How many food in terms of Kg?")
+print ("Why are you In a mood, have you gone to a therapist? there's no shame in that.")
+water_pack = input("How many water do you need?('in terms of Liters')")
+print("https://www.youtube.com/watch?v=S3fJoATTUhk")
+#Display the supply data
+print("supply list")
+print("OxygenTanks:" + (OxygenTanks))
+print("food_pack:" + (food_pack))
+print("water_pack:" + (water_pack))
+
+#list them again
+print("supply list")
+print(f"OxygenTanks:" + (OxygenTanks))
+print(f"food_pack:" + (food_pack))
+print(f"water_pack:" + (water_pack))
