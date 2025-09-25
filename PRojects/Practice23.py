f3bf40262cad9d294ece2cfe5072ab86225a0736
reoiuiourwrewiuirqirweurui <821318@stma.k12.mn.us>
2025-09-25 13:15:45 -0500

    Practice23.py

---
+++ PRojects/Practice23.py
@@ -0,0 +1,14 @@
+x = 3
+z = 80
+print(x*z)
+lowercase = False
+UPPERCASE = False
+UpperCamelCase = False
+lowerCamelCase = False
+snake_case = True
+SCREAMING_SNAKE_CASE = False
+#Rules
+#1. Shit should be clear and short as possible.
+
+
+
