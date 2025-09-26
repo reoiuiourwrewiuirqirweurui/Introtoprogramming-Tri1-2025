93871a156ebea1f6757417734f1843737a94c438
reoiuiourwrewiuirqirweurui <821318@stma.k12.mn.us>
2025-09-26 14:07:50 -0500

    BenMYu

---
+++ PRojects/BenMYu.py
@@ -0,0 +1,11 @@
+Favourite = input("What is your current favourite animal(s)")
+Skin = input("What type of skin do they have (E.g Scales, feathery.)?")
+Colour = input("What colour does your animal have\n> ")
+FacialFeatures = input ("What facial features does your animal have (E.G does it have eyes, mouth piece.)")
+Species = input("What type species is your animal in?")
+Diet = input("What type of food do they eat in their lives?")
+Location = input("Which continent/country does your animal live in?")
+Sound = input("which kind of sound does the animal make?")
+PhysicalFeatures = input("How tall is your animal?")
+Pet = input("IS it your pet or out in the wild?")
+Summary = "Favourite ,Skin, Colour, FacialFeatures, Species, Diet, Location, PhysicalFeatures, Sound, Pet"
