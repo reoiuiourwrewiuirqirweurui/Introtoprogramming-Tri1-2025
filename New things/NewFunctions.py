c11c477661c21d0dfeae2c8dcfefcf2c43beab9c
reoiuiourwrewiuirqirweurui <821318@stma.k12.mn.us>
2025-10-01 13:59:00 -0500

    NewFunctions

--- New things/NewFunctions.py
+++ New things/NewFunctions.py
@@ -2,3 +2,8 @@ def calculate_tax(Item,Price,Rate):
     return (0.01 *(Price*Rate)) + (Item + Price)
 print (calculate_tax(200,10,3/8))
 
+#Creat A function caled Add_5_functions that has 5 parametrss
+#-One of each number.
+#print 5 times
+#5 different arguments
+
