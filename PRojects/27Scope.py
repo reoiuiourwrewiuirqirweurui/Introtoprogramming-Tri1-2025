2684e3642d003c355f021f026ab7dbc7baa309f1
reoiuiourwrewiuirqirweurui <821318@stma.k12.mn.us>
2025-10-02 13:36:56 -0500

    27Scope

---
+++ PRojects/27Scope.py
@@ -0,0 +1,30 @@
+def add(x,y):
+    if(-y):
+        return(x-y)
+        
+    return(x+y)
+def sub(z,a):
+    if(-a):
+        return(z+a)
+    else:
+        return(z-a)
+def multiply(b,c):
+    return(b*c)
+def divide(d,e):
+    if(e == 0):
+        return("Error! Error! Not a function!")
+    return(d/e)
+def exponents(f,g,):
+    if(g < 0):
+        return(f/f**g)
+    return(f**g)
+def modules(h,i):
+    if(i == 0):
+        return("Bye have a great time!")
+    else:(h%i)
+def Yes(j,k):
+    if(k == 0):
+        return("Why are always in a mood? what you gone to a therapist?")
+    else:(j//k)
+#Defo Crying emoji cause the code is inspired by someone.
+
