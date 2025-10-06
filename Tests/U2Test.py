8027c92f51ed5601e69aca91e3765a69fb5d1220
reoiuiourwrewiuirqirweurui <821318@stma.k12.mn.us>
2025-10-06 13:36:41 -0500

    U2Test

---
+++ Tests/U2Test.py
@@ -0,0 +1,15 @@
+def AddThreeLetters(x,y,z):
+    print(x+y+z)
+    AddThreeLetters(23,4,6)
+
+
+FirstWord = str(input("What's your first word (Verb)?\n"))
+Secondword = str(input("What's your second word (Verb)?\n"))
+ThirdWord = str(input("What is your third word (Verb)?\n"))
+print(+FirstWord, +Secondword, +ThirdWord)
+
+def dataThree():
+    word = input("put a word in.")
+    numb = input("Put a integer in")
+    deci = input("Put a Float in")
+    print(deci + numb + word)
