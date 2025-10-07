200889d752421a1d8ed9a428f29f1b41ffd556d1
reoiuiourwrewiuirqirweurui <821318@mystma.org>
2025-10-07 13:57:02 -0500

    Boolean

---
+++ Assignments (PLEASE LOOK IN HERE)/Boolean.py
@@ -0,0 +1,5 @@
+O = int(input("Give number between 5 and 10"))
+print(5<=O<=10)
+A = float(int(input("Give me a number between 1 and 0")))
+print(0<=A<=1)
+
--- Tests/U2Test.py
+++ Tests/U2Test.py
@@ -1,15 +1,18 @@
 def AddThreeLetters(x,y,z):
+    x = int(input("Number goes here\n"))
+    y = int(input("Number goes here\n"))
+    z = int(input("Number goes here\n"))
     print(x+y+z)
-    AddThreeLetters(23,4,6)
+    AddThreeLetters(x,y,z)
 
 
 FirstWord = input("What's your first word (Verb)?\n")
 Secondword = input("What's your second word (Verb)?\n")
 ThirdWord = input("What is your third word (Verb)?\n")
-print(+FirstWord, +Secondword, +ThirdWord)
+print(FirstWord + Secondword + ThirdWord)
 
 def dataThree():
     word = input("put a word in.")
     numb = input("Put a integer in")
     deci = input("Put a Float in")
-    print(deci + numb + word)
+    print(str(int(numb) + float(float) + +word))
