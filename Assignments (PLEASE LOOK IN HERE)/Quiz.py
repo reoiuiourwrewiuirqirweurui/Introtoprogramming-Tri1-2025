69a5f0592dd0c848e72dafc14884f25f2577ca71
reoiuiourwrewiuirqirweurui <821318@mystma.org>
2025-10-09 13:31:30 -0500

    Top5Favorit

---
+++ Assignments (PLEASE LOOK IN HERE)/Quiz.py
@@ -0,0 +1,15 @@
+Belgium = input("What's the capitl of belgium?")
+Teacher = input("who's the teacher of coding?")
+Touhou2 = input("who's the final characters of The story of Eastern Wonderlnd?")
+QuestionFour = input("Who is henry?")
+five = input("Last question, who's Joe?")
+
+Answers ={
+    "Belgium": "Brussels",
+    "Teacher": "Osowski",
+    "Touhou2": "Mima",
+    "Question4": "Horrid,"
+    "Five"
+
+
+}
---
+++ Practice/32Prac.py
@@ -0,0 +1,10 @@
+E = str(input("What's the password lool"))
+actual = "Mr. Osowski"
+if E == actual:
+    print("Wow no way!11!1!")
+else:
+    print("No Lol go away!!!111!!!!!!1!")
+    E()
+    
+
+        
