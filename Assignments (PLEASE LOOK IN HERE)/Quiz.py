d78c43363ceca071f73c20a313e1f3ef04831012
reoiuiourwrewiuirqirweurui <821318@mystma.org>
2025-10-09 13:43:17 -0500

    Quiz

--- Assignments (PLEASE LOOK IN HERE)/Quiz.py
+++ Assignments (PLEASE LOOK IN HERE)/Quiz.py
@@ -1,15 +1,21 @@
-Belgium = input("What's the capitl of belgium?")
-Teacher = input("who's the teacher of coding?")
-Touhou2 = input("who's the final characters of The story of Eastern Wonderlnd?")
-QuestionFour = input("Who is henry?")
-five = input("Last question, who's Joe?")
+Touhou15 = input("WHo's the extra boss of TH15?\n")
+Owen = input("who is Owen Rossing?\n")
+Touhou14 = input("What's the best shottype?\n")
+MrOsowski = input("who is the teacher of this class?\n")
+dwpk = input("what do you mean?\n")
+def score():
+    score = 0
+    if Touhou15 == "Hecatia":
+        score = score + 1
+    if Owen == "Guy in class":
+        score = score + 1
+    if Touhou14 == "MarisaB":
+        score = score + 1
+    if MrOsowski == "Mr. Osowski":
+        score = score + 1
+    if dwpk == "Oh oh oh":
+        score = score + 1
+    print("Score: " + str(score) + "/5")
 
-Answers ={
-    "Belgium": "Brussels",
-    "Teacher": "Osowski",
-    "Touhou2": "Mima",
-    "Question4": "Horrid,"
-    "Five"
 
-
-}
+        
